1d371c040a259e6333aec56de2475b0ead742927
Ayush Singh <Ayushsingh12525@gmail.com>
2021-03-09 02:20:32 +0530

    Added Location and Restaurant details

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:mytaste/model/topRestaurant.dart';
+import 'package:mytaste/utils/locator.dart';
 import 'homepage/Hometitle.dart';
 import 'homepage/homeheading.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +13,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Position location;
+  String latitude = "";
+  String longitude = "";
+  @override
+  void initState() {
+    super.initState();
+    getlocation();
+    getData();
+  }
+
+  // Funtion to get Location Latitute and Longitude
+  getlocation() async {
+    location = await determinePosition();
+    if (location == null) {
+      return "No Value";
+    }
+    latitude = location.latitude.toString() ?? "No Value";
+    longitude = location.longitude.toString() ?? "No Value";
+    setState(() {});
+  }
+
+  // Http Request
+  var url = Uri.https('developers.zomato.com', '/api/v2.1/search',
+      {'lon': '82.9621772', 'lat': '25.3553815', 'collection_id': '1'});
+  var header = {
+    "user-key": "2e2bf75126e32940a0f5c1ee00b96dea",
+    "Accept": "application/json"
+  };
+
+  // Await the http get response, then decode the json-formatted response.
+  void getData() async {
+    var response = await http.get(url, headers: header);
+    if (response.statusCode == 200) {
+      final topRestaurant = topRestaurantFromJson(response.body);
+      print(topRestaurant.restaurants[0].restaurant.name);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -18,6 +63,8 @@ class _HomePageState extends State<HomePage> {
             HomeHeading(),
             HomeTitle(),
             NearByRestuant(),
+            Text(latitude),
+            Text(longitude),
           ],
         ),
       ),
