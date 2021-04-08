ed97d184c1ebb11a7e9e440f419545bea9bfeed2
Ayush Singh <Ayushsingh12525@gmail.com>
2021-04-09 02:48:17 +0530

    Modifial HomePage Shimmer

--- lib/pages/homepage/HomePageShimmer.dart
+++ lib/pages/homepage/HomePageShimmer.dart
@@ -2,45 +2,202 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-SizedBox homepageshimmer() {
-  return SizedBox(
-      width: 200.0,
-      height: 500.0,
-      child: Shimmer.fromColors(
-          baseColor: Colors.white,
-          highlightColor: Colors.grey,
-          child: ListView(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+Padding homepageshimmer(BuildContext context) {
+  Size size = MediaQuery.of(context).size;
+  return Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Shimmer.fromColors(
+            baseColor: Colors.white,
+            highlightColor: Colors.grey,
+            child: Container(
+              width: 160,
+              height: 200,
+              child: GridView(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
                 children: [
                   Container(
-                    color: Colors.black,
-                    height: 10,
-                    width: 200,
-                  ).p12(),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                          topRight: Radius.circular(6)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            width: 100, height: 100, child: CircleAvatar()),
+                        Divider(),
+                        SizedBox(
+                          height: 15,
+                          child: Container(
+                            width: 100,
+                            height: 12,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ).px12().py4(),
                   Container(
-                    color: Colors.black,
-                    height: 10,
-                    width: 200,
-                  ).p12(),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                          topRight: Radius.circular(6)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            width: 100, height: 100, child: CircleAvatar()),
+                        Divider(),
+                        SizedBox(
+                          height: 20,
+                          child: Container(
+                            width: 100,
+                            height: 12,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ).px12().py4(),
                   Container(
-                    color: Colors.black,
-                    height: 10,
-                    width: 200,
-                  ).p12(),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                          topRight: Radius.circular(6)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            width: 100, height: 100, child: CircleAvatar()),
+                        Divider(),
+                        SizedBox(
+                          height: 15,
+                          child: Container(
+                            width: 100,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ).px12().py4(),
                   Container(
-                    color: Colors.black,
-                    height: 10,
-                    width: 200,
-                  ).p12(),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                          topRight: Radius.circular(6)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            width: 100, height: 100, child: CircleAvatar()),
+                        Divider(),
+                        SizedBox(
+                          height: 15,
+                          child: Container(
+                            width: 100,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ).px12().py4(),
                   Container(
-                    color: Colors.black,
-                    height: 10,
-                    width: 200,
-                  ).p12(),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                          topRight: Radius.circular(6)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            width: 100, height: 100, child: CircleAvatar()),
+                        Divider(),
+                        SizedBox(
+                          height: 15,
+                          child: Container(
+                            width: 100,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ).px12().py4(),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                          topRight: Radius.circular(6)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            width: 100, height: 100, child: CircleAvatar()),
+                        Divider(),
+                        SizedBox(
+                          height: 15,
+                          child: Container(
+                            width: 100,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ).px12().py4(),
                 ],
-              )
-            ],
-          )));
+              ),
+            ))),
+  );
 }
